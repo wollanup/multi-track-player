e204878b6748b7e3a10ed347eccfc33b07da635f
Steve Mareigner <steve@eukles.com>
2026-01-23 17:44:12 +0100

    chore(css): Forgot a file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
         categories: ['music', 'entertainment', 'utilities']
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
